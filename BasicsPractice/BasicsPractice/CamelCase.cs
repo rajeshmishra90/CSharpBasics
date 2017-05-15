cd33cd4527c6b09497e941e7dd89f5a049fd65f2
MishraRajesh <rajeshmishra90@gmail.com>
2017-05-15 11:00:36 +0530

    Adde new problem

---
+++ BasicsPractice/BasicsPractice/CamelCase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicsPractice
+{
+    public class CamelCase
+    {
+        public static void MainCC(string[] args)
+        {
+            string s = Console.ReadLine();
+            int currIndex = 0;
+            int i = 0;
+            int count = 1;
+            foreach (var item in s)
+            {
+                if (Char.IsUpper(item))
+                {
+                    count++;
+                    //var str = s.Substring(currIndex, i- currIndex);
+                    //Console.WriteLine(str);
+                    currIndex = i;
+                }
+                i++;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
